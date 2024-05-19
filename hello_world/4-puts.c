b9e9d50cce520d2c1e9e915d6dd2e5103811e85b
alex <natebarrow1@gmail.com>
2024-05-19 23:17:32 +0000

    added comments

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*this is a comment*/
+/*
+ * main - Entry poin
+ * 
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
