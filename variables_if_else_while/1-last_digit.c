b2bf4ebdd17bef50b039ecb60005fe966a8ad52b
alex <natebarrow1@gmail.com>
2024-05-20 22:33:44 +0000

    not betty compliant

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -20,11 +20,11 @@ int main(void)
 	last_digit = n % 10;
 
 	printf("Last digit of %d is %d and is ", n, last_digit);
-	if(last_digit > 5)
+	if (last_digit > 5)
 	{
 		printf("greater than 5\n");
 	}
-	else if(last_digit == 0)
+	else if (last_digit == 0)
 	{
 		printf("0\n");
 	}
