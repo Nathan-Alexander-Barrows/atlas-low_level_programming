8f80240d5ab0fa5fa0fc2a32c05f3a5780cb9946
alex <natebarrow1@gmail.com>
2024-05-20 21:53:02 +0000

    fixed termination error

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 
-	printf("Last digit of $d is %d and is ", n, last_digit");
+	printf("Last digit of $d is %d and is ", n, last_digit);
 	if(last_digit > 5)
 	{
 		printf("Greater than 5 \n");
