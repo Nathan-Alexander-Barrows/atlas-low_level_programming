92c77a95d6adbeb36da87e1a34a571d9b78f7b03
alex <natebarrow1@gmail.com>
2024-05-21 23:18:12 +0000

    made changes

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -9,16 +9,12 @@
  */
 int main(void)
 {
-    putchar('_');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
-
+    char *str = "_putchar";
+    while (*str)
+    {
+        _putchar(*str);
+        str++;
+    }
+    _putchar('\n');
     return (0);
 }
-
