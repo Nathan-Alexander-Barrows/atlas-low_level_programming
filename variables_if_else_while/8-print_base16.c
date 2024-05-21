f6eb5dff9528b8b84be23f1283ffb233703f7622
alex <natebarrow1@gmail.com>
2024-05-21 03:30:25 +0000

    added 8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all the numbers of base 16 in lowercase,
+ * followed by a new line.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+	for (num = 0; num < 16; num++)
+	{
+		if (num < 10; num++)
+			putchar(num + '0');
+		else
+			putchar(num - 10 + 'a');
+	}
+	putchar('\n');
+
+	return (0);
+}
