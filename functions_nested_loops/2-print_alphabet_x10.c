5f537e87a45b58e6fbc2780a79f92bbca572caad
alex <natebarrow1@gmail.com>
2024-05-21 23:57:54 +0000

    betty fix

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,14 @@
 void print_alphabet_x10(void)
 {
 	char letter;
-    int i;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-	    for (letter = 'a'; letter <= 'z'; letter++)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	_putchar(letter);
 	}
 	_putchar('\n');
-    }
+	}
 }
