4b5bc7ca8b6b44298e3edecc019a8c6563e4be96
alex <alexbarrows@gmail.com>
2024-05-28 16:22:36 +0000

    bug fix

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -4,7 +4,7 @@
  * print_square - prints a square, followed by a new line
  * @size: size of the square
  *
- * Description: If size is 0 or less, the function should print only a new line.
+ * Description: If size is 0 or less, the function should print only a new line
  * Use the character # to print the square.
  */
 void print_square(int size)
