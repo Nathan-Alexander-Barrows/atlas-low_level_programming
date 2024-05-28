b5154d7e6731c64f46a7fc39b69af3c286c3a7d3
alex <alexbarrows@gmail.com>
2024-05-28 05:06:20 +0000

    changed 0-isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
-/* Function definition */
-int _isupper(int c) {
-    /* Check if the character is within the ASCII range of uppercase letters */
-    if (c >= 'A' && c <= 'Z') {
-        return 1;
-    } else {
-        return 0;
+/* Function definition
+ * Check if character is within the ASCII range of uppercase letters
+ */
+int _isupper(int c) 
+{
+    if (c >= 'A' && c <= 'Z') 
+    {
+        return (1)
+    } 
+    else 
+    {
+        return (0)
     }
 }
