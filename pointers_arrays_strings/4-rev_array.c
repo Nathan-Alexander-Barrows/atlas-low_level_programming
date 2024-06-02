d776b7283bc25311d4a334611434fdf83be27fe3
alex <alexbarrows@gmail.com>
2024-06-02 20:55:58 +0000

    added a description

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,13 @@
 #include "main.h"
 /**
+ * reverse_array - Reverses the content of an array of integers
+ * @a: A pointer to the first element of the array to be reversed
+ * @n: The number of elements in the array
+ *
+ * Dscription: This function takes an array of intengers and its length
+ * and reverses the order of the elements in the array
+ * It swaps elements from the start and end of the array, moving towards
+ * the center, until the netire array is reversed
  */
 void reverse_array(int *a, int n)
 {
