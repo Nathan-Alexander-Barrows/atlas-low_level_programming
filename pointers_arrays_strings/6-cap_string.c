5b1bb44b5c60be6167d3fbac17afcc0b409a2ebf
alex <alexbarrows@gmail.com>
2024-06-03 02:55:45 +0000

    added 6-cap_strings.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * is_separator - checks if a character is a word separator
+ * @c: the character to check
+ *
+ * Return: 1 if the character is a separator, 0 otherwise
+ */
+int is_separator(char c)
+{
+	char separators[] = " \t\n,;.!?\"(){}";
+	int i;
+
+	for (i = 0; separators[i] != '\0'; i++)
+	{
+		if (c == separators[i])
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: the string to capitalize
+ *
+ * Return: pointer to the capitalized string
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 || is_separator(str[i - 1]))
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - ('a' - 'A');
+			}
+		}
+	}
+	return (str);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,5 +19,7 @@ char *_strncpy(char *dest, const char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
+char *cap_string(char *str);
+int is_separator(char c);
 
 #endif /* MAIN_H */
