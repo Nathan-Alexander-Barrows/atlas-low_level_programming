d7041ec6b1f285874494756af4fb640e7ae2da86
alex <alexbarrows@gmail.com>
2024-06-03 03:02:52 +0000

    fixed spaces at beginnning of lines

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,19 +9,19 @@
 char *leet(char *str)
 {
 	int i, j;
-    char leet_map[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-    char leet_replace[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	char leet_map[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char leet_replace[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-	    for (j = 0; j < 10; j++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == leet_map[j])
-	    {
-		    str[i] = leet_replace[j];
-		break;
-	    }
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == leet_map[j])
+			{
+				str[i] = leet_replace[j];
+				break;
+			}
+		}
 	}
-    }
-    return (str);
+	return (str);
 }
