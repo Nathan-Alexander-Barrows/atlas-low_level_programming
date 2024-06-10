941ee0812820d06649c8c42ebb4dba5f69e3ea95
alex <alexbarrows@gmail.com>
2024-06-10 00:58:42 +0000

    freaking betty!

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -17,10 +17,10 @@ int is_prime_helper(int n, int i)
 		return (1);
 	if (n % i == 0)
 		return (0);
-	return is_prime_helper(n, i + 1);
+	return (is_prime_helper(n, i + 1));
 }
 
 int is_prime_number(int n)
 {
-	return is_prime_helper(n, 2);
+	return (is_prime_helper(n, 2));
 }
